b9d5f27a2ac4778cdc99d8d6a24d78aa86063e5c
V1kata <viktor.burboran@gmail.com>
2025-04-14 17:41:10 +0300

    added a new style between m and xl screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,7 +54,10 @@ module.exports = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		screens: {
+			between: {'min': '764px', 'max': '1000px'}
+		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
