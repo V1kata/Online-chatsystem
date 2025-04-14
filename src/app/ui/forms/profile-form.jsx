f9f0bfd7968fab1df77b5ac34e1a2e9f63e0ed0d
V1kata <viktor.burboran@gmail.com>
2025-04-14 17:40:47 +0300

    error handling

--- src/app/ui/forms/profile-form.jsx
+++ src/app/ui/forms/profile-form.jsx
@@ -2,10 +2,13 @@
 import { useUser } from "@/app/context/UserContext";
 import { useRouter } from "next/navigation";
 import { updateProfile } from "@/lib/authentication";
+import { useState } from "react";
 
 export function ProfileForm() {
   const { userData, setUserData } = useUser();
-  const router = useRouter()
+  const router = useRouter()    
+  const [error, setError] = useState(null);
+  
   async function handleUpdate(formData) {
     const { username, profileImageUrl } = Object.fromEntries(formData);
     const userProfile = { username, profileImageUrl, user_id: userData.user.id, email: userData.user.email};
@@ -21,7 +24,8 @@ export function ProfileForm() {
 
       router.push("/chats");
     } catch (err) {
-      console.error(err);
+      setError(err.message.split(":")[1].trim());
+      return;
     }
   }
 
@@ -40,6 +44,7 @@ export function ProfileForm() {
         placeholder="Profile Image URL"
         className="w-full mb-4 p-2 border rounded-md focus:outline-none"
       />
+      {error && <p className="text-red-500 mb-4">{error}</p>}
       <button className="w-full py-2 bg-green-600 text-white rounded-md hover:bg-green-700">Save Profile</button>
     </form>
   )
--- src/app/ui/forms/register-form.jsx
+++ src/app/ui/forms/register-form.jsx
@@ -1,21 +1,24 @@
 "use client"
 import { registerUser } from "@/lib/authentication";
 import { useUser } from "@/app/context/UserContext";
+import { useState } from "react";
 
 export function RegisterForm({ onSuccess }) {
     const { setUserData } = useUser();
+    const [error, setError] = useState(null);
     async function signIn(formData) {
         try {
             const data = await registerUser(Object.fromEntries(formData));
-            
+
             if (data.error) {
                 throw new Error(data.error);
             }
-            
+
             setUserData(data);
             onSuccess();
         } catch (err) {
-            console.error(err);
+            setError(err.message.split(':')[1].trim());
+            return;
         }
     }
 
@@ -34,6 +37,7 @@ export function RegisterForm({ onSuccess }) {
                 name="password"
                 className="w-full mb-4 p-2 border rounded-md focus:outline-none"
             />
+            {error && <p className="text-red-500 mb-4">{error}</p>}
             <button className="w-full py-2 bg-green-600 text-white rounded-md hover:bg-green-700">Register</button>
         </form>
     )
